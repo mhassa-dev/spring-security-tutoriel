f79e4607a84a7305d22e816c09305de72582063b
mhassa <mhassa.dev@gmail.com>
2020-08-19 20:15:53 +0200

    initial commit

---
+++ src/main/java/mh/com/project/SpSecurityApplication.java
@@ -0,0 +1,13 @@
+package mh.com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mh/com/project/SpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
