c5cdf17c0393c3b09bc63afce1e8321f71740e12
mhassa <mhassa.dev@gmail.com>
2020-08-20 08:14:14 +0200

    Ant Matchers to allow to acces to some endpoints without authorizing.

--- src/main/java/mh/com/project/security/SecurityConfig.java
+++ src/main/java/mh/com/project/security/SecurityConfig.java
@@ -16,6 +16,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
