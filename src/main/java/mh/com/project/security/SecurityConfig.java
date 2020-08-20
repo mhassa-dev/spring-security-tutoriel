cc1f304908a2940c339f5339a3b450ca1922a8c0
mhassa <mhassa.dev@gmail.com>
2020-08-20 08:05:19 +0200

    Basic auth (username, password).

---
+++ src/main/java/mh/com/project/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package mh.com.project.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by mh on 19/08/2020.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
