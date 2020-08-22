f72bc27e6a7da5ac0ad1e6d75c6d810a98a584ad
mhassa <mhassa.dev@gmail.com>
2020-08-22 19:09:54 +0200

    - Log out

--- src/main/java/mh/com/project/security/SecurityConfig.java
+++ src/main/java/mh/com/project/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -49,7 +50,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe().tokenValiditySeconds((int) TimeUnit.MINUTES.toSeconds(10)).key("GZ=#^N^xjM=u%@");
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.MINUTES.toSeconds(10)).key("GZ=#^N^xjM=u%@")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
